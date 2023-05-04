09f302cf660f7a1dadfc4557e5140a640917767b
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-04 11:50:11 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,3 @@
-
-
 import React,{useState,useEffect} from "react"
 import { Link } from 'react-router-dom';
 import AddAnimal from "./AddAnimal"
@@ -8,15 +6,7 @@ import UpdateAnimal from "./UpdateAnimal";
 
 function Home(){
   const [animals,setAnimals] = useState([])
-  const [totalLikes,setTotalLikes] =useState(0)
   const[selectedBreed,setSelectedBreed]= useState("All")
-   const [showForm, setShowForm] = useState(false);
-
-  function handleUpdateClick() {
-    setShowForm(true);
-  }
-  // const [showDetails, setShowDetails] = useState(false);
-
   const allBreeds=[
     "Dog",
     "Cat",
@@ -24,123 +14,106 @@ function Home(){
     "Rabbit",
     "Panda",
     "Bird",
-]
+  ]
  
-useEffect(() => {
-  // fetch animals data
-  fetch("http://localhost:3000/animals")
-    .then((response) => response.json())
-    .then((data) =>
-      setAnimals(
-        data.map((animal) => ({
-          ...animal,
-          liked: false, // here we are adding the like state to each animal
-        }))
+  useEffect(() => {
+    // fetch animals data
+    fetch("http://localhost:3000/animals")
+      .then((response) => response.json())
+      .then((data) =>
+        setAnimals(
+          data.map((animal) => ({
+            ...animal,
+               likes: animal.likes || 0, 
+            
+          }))
+        )
       )
-    )
+  }, [])
 
-  // fetch likes data
-  fetch("http://localhost:3000/likes")
-    .then((response) => response.json())
-    .then((data) => {
-      const likes = data.reduce(
-        (acc, cur) => ({ ...acc, [cur.animalId]: cur.likes }),
-        {}
-      )
-      setTotalLikes(likes)
-    })
-}, [])
-
-
-  
-  //handling the delete function
   function handleDelete(id){
-fetch(`http://localhost:3000/animals/${id}`,{
-  method:"DELETE",
-})
-.then(()=>{
-  setAnimals(animals.filter((animal)=> animal.id !== id))
-})
-  }
-  //handling the like function for the likes in the web app
-  
-  function handleLike(id) {
-    setTotalLikes((prevLikes) => {
-      const newLikes = { ...prevLikes, [id]: (prevLikes[id] || 0) + 1 };
-      fetch(`http://localhost:3000/animals/${id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          likes: newLikes[id],
-        }),
-      }).then((response) => response.json());
-      return newLikes;
+    fetch(`http://localhost:3000/animals/${id}`,{
+      method:"DELETE",
     })
-  
-    setAnimals((prevAnimals) =>
-      prevAnimals.map((animal) =>
-        animal.id === id ? { ...animal, liked: !animal.liked } : animal
-      )
-    );
+    .then(()=>{
+      setAnimals(animals.filter((animal)=> animal.id !== id))
+    })
   }
   
-
-  
-  //on line 41 we are displaying the number of total likes
   function fetchSearch(search){
-const results=animals.filter(animal=>animal.name === search)
-setAnimals(results)
+    const results=animals.filter(animal=>animal.name === search)
+    setAnimals(results)
   }
-  function handleBuy(id) {
-  const animal = animals.find(animal => animal.id === id);
-
-  fetch(`http://localhost:3000/animals/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      ...animal,
-      AvailabilityStatus: "not available already bought",
-      disabled:true,
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    setAnimals(animals.map(animal => {
-      if (animal.id === id) {
-        return {
-          ...animal,
-          AvailabilityStatus: data.AvailabilityStatus,
-          disabled:true,
-        };
-      } else {
-        return animal;
-      }
-    }));
-  })
   
-}
-function handleBreed(e){
-  setSelectedBreed(e.target.value)
-}
-//   function handleShowDetails(id) {
-//   setAnimals(prevAnimals =>
-//     prevAnimals.map(animal =>
-//       animal.id === id ? { ...animal, showDetails: !animal.showDetails } : animal
-//     )
-//   );
-// }
+  function handleBuy(id) {
+    const animal = animals.find(animal => animal.id === id);
 
+    fetch(`http://localhost:3000/animals/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        ...animal,
+        AvailabilityStatus: "not available already bought",
+        disabled:true,
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      setAnimals(animals.map(animal => {
+        if (animal.id === id) {
+          return {
+            ...animal,
+            AvailabilityStatus: data.AvailabilityStatus,
+            disabled:true,
+          };
+        } else {
+          return animal;
+        }
+      }));
+    })
+  }
+  function handleLike(id) {
+    const animal = animals.find((animal) => animal.id === id);
 
+    fetch(`http://localhost:3000/animals/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...animal,
+        likes: animal.likes + 1,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAnimals(
+          animals.map((animal) => {
+            if (animal.id === id) {
+              return {
+                ...animal,
+                likes: data.likes,
+              };
+            } else {
+              return animal;
+            }
+          })
+        );
+      });
+  }
+  
+  function handleBreed(e){
+    setSelectedBreed(e.target.value)
+  }
+  
   return(
     <div className="home-animal-card" >
       <header> THE PET KINGDOM </header>
       <Search getSearch={fetchSearch}/>
       <label className="filter">
-  Filter by breed:
+  Choose Animal by breed:
   <select value={selectedBreed} onChange={handleBreed} className="select">
     {allBreeds.map((breed) => (
       <option key={breed} value={breed}>
@@ -159,7 +132,7 @@ function handleBreed(e){
     {animals  
       .filter((animal) => selectedBreed === "All" || animal.breed === selectedBreed)
       .map((animal) => {
-        const likeButton = animal.liked ? "Liked" : "Like";
+      
         return (
           <div className={`animal-card ${animal.disabled ? "disabled" : ""}`}>
             <img src={animal.image} alt={animal.name} />
@@ -177,17 +150,15 @@ function handleBreed(e){
             <button type="submit" onClick={() => handleBuy(animal.id)}>
               Buy
             </button>
-            <button onClick={() => handleLike(animal.id)}>{likeButton}</button>
-            <p>{totalLikes[animal.id] || 0} likes</p>
+            
             <button onClick={() => handleDelete(animal.id)}>DELETE</button>
+            <button onClick={() => handleLike(animal.id)}>
+                      {`Like (${animal.likes})`}
+                        </button>
         
              {/* <button onClick={()=>handleShowDetails(animal.id)}>
                 {showDetails ? "Show Less" : "Show More"}
               </button> */}
-              <button onClick={handleUpdateClick}>Update an Animal</button>
-      {showForm && (
-        <UpdateAnimal animals={animals} setAnimals={setAnimals} />
-      )}
           </div>
         );
       })}
@@ -195,7 +166,7 @@ function handleBreed(e){
 
   <AddAnimal animals={animals} setAnimals={setAnimals}/>
   
- 
+ <UpdateAnimal animals={animals} setAnimals={setAnimals} />
   <footer>All rights reserved@2023</footer>
 </div>
 );
